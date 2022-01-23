36969f2e1f11af3256e7c19764dd924c05f23cf3
MoneshMS <moneshsanju@gmail.com>
2022-01-23 18:23:14 +0530

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material.module';
+import { PrimengModule } from './primeng.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
