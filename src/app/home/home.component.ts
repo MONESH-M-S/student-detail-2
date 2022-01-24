e1618114b6c90bac6165b133ec35e9420a8d2012
MoneshMS <moneshsanju@gmail.com>
2022-01-24 13:05:07 +0530

    Added user-login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MessageService } from 'primeng/api';
+import { HomeService } from './home.service';
 import { SignupComponent } from './signup/signup.component';
 
 @Component({
@@ -10,11 +12,29 @@ import { SignupComponent } from './signup/signup.component';
 })
 export class HomeComponent implements OnInit {
   errorMsg = '';
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private homeService: HomeService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {}
 
-  onSubmit(form: NgForm) {}
+  onSubmit(form: NgForm) {
+    const loginData = {
+      email: form.value.email,
+      password: form.value.password,
+    };
+
+    this.homeService.userLogin(loginData).subscribe((res) => {
+      if (res.user === null) {
+        return this.messageService.add({
+          severity: 'error',
+          summary: res.message,
+        });
+      }
+    });
+  }
 
   onSignUpClicked() {
     this.dialog.open(SignupComponent, {
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +10,15 @@ export class HomeService {
   BACKEND_URL = environment.backend_url;
   constructor(private http: HttpClient) {}
 
+  userLogin(form: { email: string; password: string }) {
+    return this.http.post<{ user: User; message: string }>(
+      `${this.BACKEND_URL}user/login`,
+      form
+    );
+  }
+
   userSignup(form: FormData) {
-    return this.http.post<{ user: any; message: string }>(
+    return this.http.post<{ user: User; message: string }>(
       `${this.BACKEND_URL}user/signup`,
       form
     );
