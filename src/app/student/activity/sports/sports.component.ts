ed34cf78b03eee01cd5a72c628250fe76b29204a
MoneshMS <moneshsanju@gmail.com>
2022-01-30 16:25:49 +0530

    Added form for sports & vac component

--- src/app/student/activity/sports/sports.component.ts
+++ src/app/student/activity/sports/sports.component.ts
@@ -1,15 +1,103 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { StudentService } from '../../student.service';
 
 @Component({
   selector: 'app-sports',
   templateUrl: './sports.component.html',
-  styleUrls: ['./sports.component.scss']
+  styleUrls: ['./sports.component.scss'],
 })
 export class SportsComponent implements OnInit {
+  id: string;
+  name: string;
+  activityForm: FormGroup;
+  date: string;
+  imageDisplay: string;
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private studentService: StudentService,
+    private location: Location,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this._initForm();
+    this.date = new Date().toDateString();
+    this.route.params.subscribe((params) => {
+      if (params) {
+        this.id = params['id'];
+        this.studentService.getStudentDataById(this.id).subscribe((res) => {
+          if (res.user[0]._id) {
+            this.name = res.user[0].username;
+          }
+        });
+      }
+    });
   }
 
+  goBack() {
+    this.location.back();
+  }
+
+  onUpload(event: any) {
+    const file = event.files[0];
+    this.activityForm.patchValue({ image: file });
+    this.activityForm.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageDisplay = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onClear() {
+    window.location.reload();
+  }
+
+  onSubmit() {
+    this.isLoading = true;
+    if (this.activityForm.invalid) {
+      return;
+    }
+
+    const f = this.activityForm.value;
+
+    const form = {
+      name: f.name,
+      location: f.location,
+      prize: f.prize,
+      game: f.game,
+      startDate: f.startDate,
+      endDate: f.endDate,
+      mark: +f.mark,
+      image: f.image,
+      type: 'placement',
+      uploadedDate: this.date,
+    };
+
+    this.studentService
+      .uploadStudentActivity(this.id, form)
+      .subscribe((res) => {
+        this.isLoading = false;
+        console.log(res);
+      });
+  }
+
+  private _initForm() {
+    this.activityForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      location: ['', [Validators.required]],
+      prize: ['', [Validators.required]],
+      game: ['', [Validators.required]],
+      startDate: ['', [Validators.required]],
+      endDate: ['', [Validators.required]],
+      mark: ['', [Validators.required]],
+      image: ['', Validators.required],
+    });
+  }
 }
