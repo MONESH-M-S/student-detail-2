f75ad3878bb726cd9ff8e159305b5fce4fc5c5d5
MoneshMS <moneshsanju@gmail.com>
2022-01-31 10:27:48 +0530

    Added delete funcitonallity for activity

---
+++ src/app/student/delete-activity-dialog/delete-activity-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-activity-dialog',
+  templateUrl: './delete-activity-dialog.component.html',
+  styleUrls: ['./delete-activity-dialog.component.scss']
+})
+export class DeleteActivityDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
