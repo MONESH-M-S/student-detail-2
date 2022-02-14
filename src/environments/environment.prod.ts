de2dc97052fad8b04d70ffa66d09151c0357f194
MoneshMS <moneshsanju@gmail.com>
2022-02-14 09:20:58 +0530

    Modified backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backend_url: 'https://student-detail-2.herokuapp.com/'
+  backend_url: 'https://student-detail-sap-2.herokuapp.com/'
   // backend_url: 'http://localhost:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backend_url: 'https://student-detail-2.herokuapp.com/',
+  backend_url: 'https://student-detail-sap-2.herokuapp.com/',
   // backend_url: 'http://localhost:3000/'
 };
 
